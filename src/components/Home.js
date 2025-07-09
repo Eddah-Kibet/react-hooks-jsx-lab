5d62a6e08a2cec95da187030d958e13f6a8f9ccc
Eddah kibet <eddahkibet20@gmail.com>
2025-07-09 12:59:52 +0300

    jsx lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const divstyle = {
+  color: 'firebrick'
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id = "home">
+      <h1 style ={divstyle}>Your {name} is a Web Developer from Your {city}</h1>
+    </div>
+  
+  );
+  
 }
 
 export default Home;
